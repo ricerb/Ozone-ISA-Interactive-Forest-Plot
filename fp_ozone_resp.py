a20f6f0cac1283d8e4cd7af81faa55bc39f5dd74
ricerb <58782045+ricerb@users.noreply.github.com>
2020-01-30 11:21:46 -0500

    Update fp_ozone_resp.py

--- fp_ozone_resp.py
+++ fp_ozone_resp.py
@@ -4,6 +4,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import dash_table
 import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
@@ -14,62 +15,98 @@ dataset = str(ROOT / 'forest_plot_epi_resp_data.csv')
 assert Path(dataset).exists()
 
 df = pd.read_csv(dataset)
-columns = list(df.columns.values)
-dimensions = columns[0:9]
+df['Exposure Quartile'] = df['Exposure Quartile'].str.strip('](')
+df['Exposure Quartile'] = df['Exposure Quartile'].replace('18.398999999999997, 32.65', '18.4. 32.65')
+
+dcol1 = ['Health Outcome Measure', 'Study Name']
+dcol2 = ['Study Year','Study Type', 'City',]
+dcol3 = ['Age Group', 'Exposure Quartile', 'Lag']
+
+dimensions = dcol1 + dcol2 + dcol3
 
 
 app = dash.Dash(__name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"])
 server = app.server
 app.config.suppress_callback_exceptions = True
 
-app.layout = html.Div([html.H3('Ozone ISA Interactive Forest Plot Prototype'),
+def createselector(dimensions):
+    return [html.P([d + ":", dcc.Dropdown(id=d,
+            options=[dict(label=x, value=x) for x in df[d].unique()],
+            value=[x for x in df[d].unique()],
+            multi=True)]) 
+            for d in dimensions]
 
+selectors = [createselector(dcol1), createselector(dcol2), createselector(dcol3)]
+
+selectors.append(
+     html.P(["Sort by:", dcc.Dropdown(id="sort-by",
+                    options=[dict(label="Study Year", value='year'),
+                        dict(label="Study Name", value='name'),
+                        dict(label="Exposure Level", value='mean ppb')],
+                    value="b",
+                    multi=False)])
+)
+            
+app.layout = html.Div([
+    
+    html.H3('Ozone ISA Interactive Forest Plot Prototype v2'),
+    
     html.Div([
         html.Div(
-                [
-                    html.P([d + ":", dcc.Dropdown(id=d,
-                    options=[dict(label=x, value=x) for x in df[d].unique()],
-                    value=[x for x in df[d].unique()],
-                    multi=True)])
-                    for d in dimensions
-                ],
-                style={"width": "100%", "float": "left", "display": "inline-block"},
-        )
+                s,
+                style={"width": "33%", "float": "left", "display": "inline-block"},
+        ) for s in selectors
     ]),
-    dcc.Graph(id='output-graph', style={"width": "100%", "display": "inline-block"})
+    
+    dcc.Graph(id='output-graph', style={"width": "100%", "display": "inline-block"}),
+
+    html.H5('Input Data:'),
+
+    dash_table.DataTable(
+        id='output-table',
+        style_table={'overflowX': 'scroll',
+            'overflowY': 'scroll',
+            'maxHeight': '500px'}
+    )
 
   ]
 )
 
-@app.callback(
-  Output(component_id='output-graph', component_property='figure'),
-  [Input(component_id=d, component_property='value') for d in dimensions])
+listeners = [Input(component_id=d, component_property='value') for d in dimensions]
+listeners.append(Input(component_id="sort-by", component_property='value'))
 
-def update_graph(Health_Outcome_Measure, Exposure_Timing, Study_Name, Study_Year, Study_Type,
-Location, Age, Mean_ppb, Lag):
+@app.callback([Output(component_id='output-graph', component_property='figure'),
+            Output(component_id='output-table', component_property='data'),
+            Output(component_id='output-table', component_property='columns')],
+            listeners)
 
-    filterables = [Health_Outcome_Measure, Exposure_Timing, Study_Name, Study_Year, Study_Type,
-    Location, Age, Mean_ppb, Lag]
+def update_data(Health_Outcome_Measure, Study_Name, Study_Year, Study_Type,
+City, Age_Group, Exposure_Quartile, Lag, SortBy):
 
-    for f in filterables:
+    filterables = [Health_Outcome_Measure, Study_Name, Study_Year, Study_Type,
+    City, Age_Group, Exposure_Quartile, Lag]
+    
+    gdf = df
+    for i, f in enumerate(filterables):
         if type(f) != list:
             f = ['']
+        gdf = gdf[(gdf.iloc[:,i].isin(f))]
 
-    gdf = df[(df.iloc[:,0].isin(filterables[0]))&
-    (df.iloc[:,1].isin(filterables[1]))&
-    (df.iloc[:,2].isin(filterables[2]))&
-    (df.iloc[:,3].isin(filterables[3]))&
-    (df.iloc[:,4].isin(filterables[4]))&
-    (df.iloc[:,5].isin(filterables[5]))&
-    (df.iloc[:,6].isin(filterables[6]))&
-    (df.iloc[:,7].isin(filterables[7]))&
-    (df.iloc[:,8].isin(filterables[8]))]
 
-    gdf['ID'] = gdf['Study Name'].astype(str) + ' (' + gdf['Age'].astype(str) + ')'
+    if SortBy=='year':
+        gdf = gdf.sort_values(['Study Year', 'Study Name'], ascending = False)
+    elif SortBy == 'name':
+        gdf = gdf.sort_values(['Study Name', 'Study Year'], ascending = False)
+    elif SortBy == 'mean ppb':
+        gdf = gdf.sort_values(['mean ppb est'], ascending = True)
+    
+    gdf['ID'] = gdf['Study Name'].astype(str) + ' (' + gdf['Result Detail'].astype(str) + ')' +' (' + gdf['Age'].astype(str) + ')'
+    gdf['ID'].replace('\s\(nan\)', '', inplace = True, regex = True)
+
 
     fig = px.scatter(gdf, y='ID', x='OR',
     error_x='Upper_Error', error_x_minus='Lower_Error',
-    hover_data = ['Health Outcome Measure', 'Exposure Timing','Study Type', 'Location', 'Age',
+    hover_data = ['Study Year', 'Health Outcome Measure', 'Exposure Timing','Study Type', 'Country', 'City', 'Age', 'Age Group',
     'Mean ppb', 'Location in 2019 Ozone ISA'])
 
     fig.add_shape(
@@ -90,9 +127,12 @@ Location, Age, Mean_ppb, Lag):
     fig.update_xaxes(showgrid = False, zeroline = False)
     fig.update_yaxes(showgrid=False)
     fig.update_traces(marker=dict(color='DarkSlateGrey', size=12, line=dict(width=2, color='black')))
-    fig.update_layout(height=1000)
+    fig.update_layout(height=1000, yaxis_title="")
 
-    return fig
+    data = gdf.to_dict('records')
+    cols = [{"name": i, "id": i} for i in gdf.columns]
+
+    return fig, data, cols
 
 
 if __name__ == "__main__":
